89c136019844de7b2cba3cfa2f8fc27a475d129c
JohnWRalph <John.Ralph.Git@gmail.com>
2023-01-10 17:10:30 -0600

    Updated fight logic, styling

--- bundle.js
+++ bundle.js
@@ -587,6 +587,9 @@ function toggleMobInfoDisplay() {
     const infoContainer = document.getElementById("mob-info-container");
     infoContainer.style.display = "Inline-block";
 
+    const infoPicture = document.getElementById("mob-picture");
+    infoPicture.style.display = "Inline-block";
+
 }
 
 module.exports = toggleMobInfoDisplay;
